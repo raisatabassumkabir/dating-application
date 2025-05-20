82e14e47fdf2f4f70267ce64bfe0b4fb72dbedcd
Raisa Tabassum Kabir <2022100000032@seu.edu.bd>
2025-05-21 00:15:23 +0600

    biye

---
+++ src/main/java/bd/edu/seu/biye_shaddi/controller/IndexController.java
@@ -0,0 +1,14 @@
+package bd.edu.seu.biye_shaddi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String indexPage(){
+        return "index";
+    }
+}
---
+++ src/test/java/bd/edu/seu/biye_shaddi/BiyeShaddiApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.edu.seu.biye_shaddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiyeShaddiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
